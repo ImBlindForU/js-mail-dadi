c4c5f464cfb70ed38d106b4717e914649cf24209
ImBlindForU <samuele.zanchi10@gmail.com>
2022-10-11 16:32:29 +0200

    esercizio mail

---
+++ mail/js/script.js
@@ -0,0 +1,26 @@
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+const accessAllow = ["zack@gmail.com", "marco@gmail.com"," gianni@gmail.com", "giorgiO@gmail.com"]
+
+
+const mail = prompt("qual e la tua email?")
+console.log(mail);
+
+let access = false;
+
+for (let i = 0; i < accessAllow.length; i++) {
+  const element = accessAllow[i];
+
+  if (element === mail) {
+    access = true;
+  } 
+  console.log(access)
+}
+
+if (access) {
+    alert("Congratulazione! hai ottenuto l'accesso")
+  } else {
+    alert("Ci dispiace tanto, ma non abbiamo trovato la sua mail all'interno della nostra lista riprovi con un altra mail.")
+  }
